6846764562fc5d6df7b4667c342bbe277e11e3e2
Enrico Santoemma <esantoemma@es-tek.it>
2022-05-10 19:57:54 +0200

    localstorage

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,11 +3,12 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore({
   id: 'user',
   state: () => ({
-    user: ''
+    user: localStorage.user || ''
   }),
   actions: {
     save(name: string) {
       this.user = name
+      localStorage.user = name
     }
   }
 })
